e0f4aae88b28704823da2d6ca18b9b92e87e8a38
bluewhaleio2017 <bluewhaleio2017@gmail.com>
2017-01-24 01:51:53 +0000

    first

---
+++ src/main/java/com/fmr/AP0123/App.java
@@ -0,0 +1,13 @@
+package com.fmr.AP0123;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
